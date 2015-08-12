aa90a2ea8d676ff21ee79092a6ae64980bd9d691
webbricksteam <navininbox@yahoo.com>
2015-08-12 15:20:34 -0500

    init
    adding files

---
+++ server.js
@@ -0,0 +1,5 @@
+var connect = require('connect'); 
+var serveStatic = require('serve-static'); 
+var port = 3000;
+ var app = connect(); 
+ app.use(serveStatic(__dirname)); app.listen(port); console.log('Connected via port '+port);
